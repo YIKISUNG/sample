a86a393197b0e93303495c41d146e8ca00288f85
YIKISUNG <abc0343@gmail.com>
2015-05-26 22:14:32 +0900

    살짱 수정해따

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -4,6 +4,6 @@ public class Sample
 	public static void main(String[] args)
 	{
 		System.out.println("toavmf");
-		System.out.println("커밋커밋!!");
+		System.out.println("커밋커밋!! @@@@@@@@@@@@@@@@@@@2");
 	}
 }
